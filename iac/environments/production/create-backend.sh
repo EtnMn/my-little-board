55e7667abb51e859e30e90b60b8fafdc932a3ff1
EtnMn <etienne@menou.fr>
2025-01-21 13:57:37 +0000

    feat(ci): user assigned managed identity for github actions script

--- iac/environments/production/create-backend.sh
+++ iac/environments/production/create-backend.sh
@@ -5,8 +5,10 @@ TF_STORAGE_ACCOUNT=stmlbstate${ENV,,}$RANDOM
 CONTAINER_NAME=tfstate
 LOCK_NAME=delete-lock
 
+# Create resource group.
 az group create --name $RESOURCE_GROUP_NAME --location $LOCATION
 
+# Create storage account.
 az storage account create \
   --resource-group $RESOURCE_GROUP_NAME \
   --name $TF_STORAGE_ACCOUNT \
@@ -15,11 +17,15 @@ az storage account create \
   --tags 'ApplicationName=MLB' 'Env='${ENV} \
   --encryption-services blob
 
+# Enable soft delete.
 az storage account blob-service-properties update --account-name $TF_STORAGE_ACCOUNT \
     --resource-group $RESOURCE_GROUP_NAME \
     --enable-delete-retention true \
     --delete-retention-days 7
 
+# Create storage container.
 ACCOUNT_KEY=$(az storage account keys list --resource-group $RESOURCE_GROUP_NAME --account-name $TF_STORAGE_ACCOUNT --query '[0].value' -o tsv)
 az storage container create --name $CONTAINER_NAME --account-name $TF_STORAGE_ACCOUNT --account-key $ACCOUNT_KEY
+
+# Lock storage account.
 az lock create --name $LOCK_NAME --resource-group $RESOURCE_GROUP_NAME --lock-type CanNotDelete --resource-type Microsoft.Storage/storageAccounts  --resource $TF_STORAGE_ACCOUNT
