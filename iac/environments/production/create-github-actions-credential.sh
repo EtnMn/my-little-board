e6591b416cf1fb70d78ba38f9b0be1066458f8ef
Etienne <etienne@menou.fr>
2025-01-24 15:27:54 +0000

    feat: add infra as code (#13)
    * feat: init iac
    
    * feat(iac): create web app
    
    * feat: add documentations
    
    * refactor(iac): move devcontainer.json to root path
    
    * style: devcontainer indentation
    
    * feat(ci): user assigned managed identity for github actions script
    
    * feat(ci): clean github actions
    
    * refactor(ci): update ci workflows
    
    * feat(ci): add checkov static code analysis for iac
    
    * refactor(iac): add azure plan properties
    
    * ci(iac): ignore checknov checks
    
    * ci(iac): init terraform on tests
    
    * ci(iac): validate terraform
    
    * ci(iac): set terraform working directory
    
    * ci: add checkov pr comment
    
    * ci: extract terraform workflow
    
    * ci: clean permissions
    
    * ci: extract unit tests workflow
    
    * ci: add working directory variable to terraform
    
    * ci: add terraform to build and deploy workflow
    
    ---------
    
    Co-authored-by: EtnMn <emenou@expertime.com>

---
+++ iac/environments/production/create-github-actions-credential.sh
@@ -0,0 +1,57 @@
+# More details on: https://yourazurecoach.com/2022/12/29/use-github-actions-with-user-assigned-managed-identity/
+ENV=Prod
+LOCATION=westeurope
+RESOURCE_GROUP_NAME=rg-mlb-${ENV,,}
+IDENTITY_NAME=id-mlb-federated-${ENV,,}
+FEDERATED_IDENTITY_NAME=fid-mlb-github-${ENV,,}
+GITHUB_ISSUER=https://token.actions.githubusercontent.com
+GITHUB_ORGANIZATION=EtnMn
+GITHUB_REPOSITORY=my-little-board
+GITHUB_ENVIRONMENT=Production
+SUBJECT=repo:${GITHUB_ORGANIZATION}/${GITHUB_REPOSITORY}:environment:${GITHUB_ENVIRONMENT}
+AUDIENCE=api://AzureADTokenExchange # This value is used to establish a connection between external workload identities (e.g. GitHub) and Microsoft Entra ID.
+
+# Create User Assigned Managed Identity.
+IDENTITY_OUTPUT=$(az identity create \
+    --name $IDENTITY_NAME\
+    --resource-group $RESOURCE_GROUP_NAME \
+    --location $LOCATION \
+    --output json)
+
+SUBSCRIPTION_ID=$(az account show --query id -o tsv)
+IDENTITY_NAME=$(echo $IDENTITY_OUTPUT | jq -r '.name')
+IDENTITY_PRINCIPAL_ID=$(echo $IDENTITY_OUTPUT | jq -r '.principalId')
+IDENTITY_CLIENT_ID=$(echo $IDENTITY_OUTPUT | jq -r '.clientId')
+IDENTITY_TENANT_ID=$(echo $IDENTITY_OUTPUT | jq -r '.tenantId')
+
+# Add environment federated credentials.
+az identity federated-credential create \
+    --name $FEDERATED_IDENTITY_NAME \
+    --identity-name $IDENTITY_NAME \
+    --resource-group $RESOURCE_GROUP_NAME \
+    --issuer $GITHUB_ISSUER \
+    --subject $SUBJECT \
+    --audiences $AUDIENCE \
+    --output none
+
+
+# Assign Contributor permissions to create Azure resources.
+az role assignment create \
+    --role "Contributor" \
+    --assignee-object-id $IDENTITY_PRINCIPAL_ID \
+    --assignee-principal-type ServicePrincipal \
+    --scope /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP_NAME \
+    --output none
+
+# Assign User Access Administrator permissions to perform role assignments on managed identities.
+az role assignment create \
+    --role "User Access Administrator" \
+    --assignee-object-id $IDENTITY_PRINCIPAL_ID \
+    --assignee-principal-type ServicePrincipal \
+    --scope /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP_NAME \
+    --output none
+
+# Add the following secrets in GitHub environment credentials.
+echo "AZURE_CLIENT_ID: ${IDENTITY_CLIENT_ID}"
+echo "AZURE_TENANT_ID: ${IDENTITY_TENANT_ID}"
+echo "AZURE_SUBSCRIPTION_ID: ${SUBSCRIPTION_ID}"
---
+++ iac/environments/production/main.tf
@@ -0,0 +1,51 @@
+# Specify the version of the AzureRM Provider to use
+terraform {
+  required_version = ">= 1.10.4"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.16.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-mlb-prod"
+    storage_account_name = "stmlbstate22472"
+    container_name       = "tfstate"
+    key                  = "mlb.tfstate"
+  }
+}
+
+# Configure the AzureRM Provider
+provider "azurerm" {
+  features {}
+  # In version 4.0 of the Azure Provider, the Azure Subscription ID is required.
+  subscription_id = "8595f4ea-053f-4d5c-854b-6db01c741588"
+}
+
+locals {
+  environment = terraform.workspace == "default" ? var.environment : terraform.workspace
+}
+
+# Get existing resource group
+data "azurerm_resource_group" "resource-group" {
+  name = "rg-${var.application-name}-${local.environment}"
+}
+
+# Set application
+module "blazor-app" {
+  source                                 = "../../modules/blazor-app"
+  environment                            = local.environment
+  application-name                       = var.application-name
+  resource-group-name                    = data.azurerm_resource_group.resource-group.name
+  asp-environment                        = var.asp-environment
+  application-insights-connection-string = module.application-insights.connection-string
+}
+
+# Set application insights.
+module "application-insights" {
+  source              = "../../modules/application-insights"
+  environment         = local.environment
+  application-name    = var.application-name
+  resource-group-name = data.azurerm_resource_group.resource-group.name
+
+}
