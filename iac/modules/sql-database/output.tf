5713e11e1cd24d3a049ba09043240aa45fe0ca16
EtnMn <etienne@menou.fr>
2025-02-03 14:23:19 +0000

    feat(iac): add sql-database module output

---
+++ iac/modules/sql-database/output.tf
@@ -0,0 +1,3 @@
+output "sql-db-connection-string" {
+  value = "Server=tcp:${azurerm_mssql_server.sql-server.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.sql-database.name};User Id=${var.web-app-name};Authentication=Active Directory Managed Identity"
+}
